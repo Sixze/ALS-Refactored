77d10c5f64de373f8f6f84cdc918541a82f4712e
Alexander Guncenko <alexivs@web.de>
2021-08-25 23:04:20 +0200

    android compile error fix

--- ALS/Source/ALS/Private/Notify/AlsAnimNotify_FootstepEffects.cpp
+++ ALS/Source/ALS/Private/Notify/AlsAnimNotify_FootstepEffects.cpp
@@ -58,7 +58,7 @@ void UAlsAnimNotify_FootstepEffects::Notify(USkeletalMeshComponent* MeshComponen
 #endif
 
 	const FAlsFootstepEffectSettings* EffectSettings{nullptr};
-	const EPhysicalSurface SurfaceType{Hit.PhysMaterial.IsValid() ? Hit.PhysMaterial->SurfaceType : SurfaceType_Default};
+	const auto SurfaceType{Hit.PhysMaterial.IsValid() ? Hit.PhysMaterial->SurfaceType : TEnumAsByte<EPhysicalSurface>(SurfaceType_Default)};
 
 	for (const auto& Effect : FootstepEffectsSettings->Effects)
 	{
