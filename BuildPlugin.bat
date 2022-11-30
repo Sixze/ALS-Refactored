c88ef3683129e8696aeaad71ffde7ea6fbe46d5b
Sixze <16257871+Sixze@users.noreply.github.com>
2022-11-30 23:47:21 +0300

    Add BuildPlugin.bat

---
+++ BuildPlugin.bat
@@ -0,0 +1,13 @@
+@echo off
+
+for /f "skip=2 tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\5.0" /v "InstalledDirectory"') do set "EngineDirectory=%%b"
+
+set AutomationToolPath="%EngineDirectory%\Engine\Build\BatchFiles\RunUAT.bat"
+set PluginPath="%cd%\ALS.uplugin"
+set OutputPath="%cd%\Build"
+
+echo Automation Tool Path: %AutomationToolPath%
+echo:
+
+call %AutomationToolPath% BuildPlugin -Plugin=%PluginPath% -Package=%OutputPath% -Rocket -TargetPlatforms=Win64
+pause
