930fc1d8cccdbb775bd03923d2b00f786fb73742
Sixze <16257871+Sixze@users.noreply.github.com>
2023-03-21 20:31:01 +0300

    Fix missing UE_INLINE_GENERATED_CPP_BY_NAME() in AlsSkeletonUtility.cpp

--- Source/ALSEditor/Private/AlsSkeletonUtility.cpp
+++ Source/ALSEditor/Private/AlsSkeletonUtility.cpp
@@ -6,6 +6,8 @@
 #include "Utility/AlsLog.h"
 #include "Utility/AlsMacros.h"
 
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AlsSkeletonUtility)
+
 #define LOCTEXT_NAMESPACE "AlsSkeletonUtility"
 
 namespace AlsSkeletonUtility
