ead7f960ad775b5d830cbba109b91853f8fe2d6e
Sixze <16257871+Sixze@users.noreply.github.com>
2023-05-28 16:26:09 +0300

    Make some functions in the AAlsCharacter class protected instead of private

--- Source/ALS/Public/AlsCharacter.h
+++ Source/ALS/Public/AlsCharacter.h
@@ -159,9 +159,10 @@ public:
 public:
 	const FGameplayTag& GetLocomotionMode() const;
 
-private:
+protected:
 	void SetLocomotionMode(const FGameplayTag& NewLocomotionMode);
 
+private:
 	void NotifyLocomotionModeChanged(const FGameplayTag& PreviousLocomotionMode);
 
 protected:
@@ -204,10 +205,9 @@ private:
 public:
 	const FGameplayTag& GetRotationMode() const;
 
-private:
+protected:
 	void SetRotationMode(const FGameplayTag& NewRotationMode);
 
-protected:
 	UFUNCTION(BlueprintNativeEvent, Category = "Als Character")
 	void OnRotationModeChanged(const FGameplayTag& PreviousRotationMode);
 
@@ -240,10 +240,9 @@ public:
 public:
 	const FGameplayTag& GetStance() const;
 
-private:
+protected:
 	void SetStance(const FGameplayTag& NewStance);
 
-protected:
 	UFUNCTION(BlueprintNativeEvent, Category = "Als Character")
 	void OnStanceChanged(const FGameplayTag& PreviousStance);
 
@@ -264,10 +263,9 @@ private:
 public:
 	const FGameplayTag& GetGait() const;
 
-private:
+protected:
 	void SetGait(const FGameplayTag& NewGait);
 
-protected:
 	UFUNCTION(BlueprintNativeEvent, Category = "Als Character")
 	void OnGaitChanged(const FGameplayTag& PreviousGait);
 
@@ -306,6 +304,7 @@ public:
 
 	void SetLocomotionAction(const FGameplayTag& NewLocomotionAction);
 
+private:
 	void NotifyLocomotionActionChanged(const FGameplayTag& PreviousLocomotionAction);
 
 protected:
