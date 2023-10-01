383e006a7a9213a8f3158cd339e948eb4dcbdac8
Sixze <16257871+Sixze@users.noreply.github.com>
2023-10-01 16:54:38 +0300

    Show inactive curves in the "Curves" debug mode category

--- Source/ALS/Private/AlsCharacter_Debug.cpp
+++ Source/ALS/Private/AlsCharacter_Debug.cpp
@@ -3,10 +3,12 @@
 #include "DisplayDebugHelpers.h"
 #include "DrawDebugHelpers.h"
 #include "Animation/AnimInstance.h"
+#include "Animation/Skeleton.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Engine/Canvas.h"
 #include "Engine/Engine.h"
+#include "Engine/SkeletalMesh.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Utility/AlsConstants.h"
 #include "Utility/AlsMath.h"
@@ -167,7 +169,7 @@ void AAlsCharacter::DisplayDebugCurves(const UCanvas* Canvas, const float Scale,
 	const auto ColumnOffset{145.0f * Scale};
 
 	TArray<FName> CurveNames;
-	GetMesh()->GetAnimInstance()->GetAllCurveNames(CurveNames);
+	GetMesh()->GetSkeletalMeshAsset()->GetSkeleton()->GetCurveMetaDataNames(CurveNames);
 
 	CurveNames.Sort([](const FName& A, const FName& B) { return A.LexicalLess(B); });
 
