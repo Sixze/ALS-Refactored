296c3a7cb98468df78f7e0855b93708462afe626
Sixze <16257871+Sixze@users.noreply.github.com>
2024-04-16 20:51:31 +0300

    Potential fix for a Clang compilation error

--- Source/ALS/Public/Nodes/AlsAnimNode_GameplayTagsBlend.h
+++ Source/ALS/Public/Nodes/AlsAnimNode_GameplayTagsBlend.h
@@ -18,6 +18,9 @@ public:
 	TArray<FGameplayTag> Tags;
 #endif
 
+public:
+	virtual ~FAlsAnimNode_GameplayTagsBlend() override = default; // Required by Clang.
+
 protected:
 	virtual int32 GetActiveChildIndex() override;
 
