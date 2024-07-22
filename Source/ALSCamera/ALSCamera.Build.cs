0b1e9bab4f488dc2146cc9077f6bc72f3529a4be
Sixze <16257871+Sixze@users.noreply.github.com>
2024-07-23 00:08:27 +0300

    Set IncludeOrderVersion to EngineIncludeOrderVersion.Unreal5_4

--- Source/ALSCamera/ALSCamera.Build.cs
+++ Source/ALSCamera/ALSCamera.Build.cs
@@ -5,7 +5,7 @@ public class ALSCamera : ModuleRules
 	public ALSCamera(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 
 		bEnableNonInlinedGenCppWarnings = true;
 		// UnsafeTypeCastWarningLevel = WarningLevel.Warning;
