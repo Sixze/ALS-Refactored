286255f21b01c2be158f68b77af2f854fc2abcd2
Sixze <16257871+Sixze@users.noreply.github.com>
2024-10-14 00:15:12 +0300

    Set IncludeOrderVersion to EngineIncludeOrderVersion.Unreal5_5

--- Source/ALSExtras/ALSExtras.Build.cs
+++ Source/ALSExtras/ALSExtras.Build.cs
@@ -5,7 +5,7 @@ public class ALSExtras : ModuleRules
 	public ALSExtras(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 
 		bEnableNonInlinedGenCppWarnings = true;
 		// UnsafeTypeCastWarningLevel = WarningLevel.Warning;
