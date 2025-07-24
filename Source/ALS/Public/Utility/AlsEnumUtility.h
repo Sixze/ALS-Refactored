e433ad2f3610c3c3249d0b381a54115addf3964c
CodingBot <20263103+ameaninglessname@users.noreply.github.com>
2025-07-25 05:22:10 +0800

    Replace std::is_enum_v with TIsUEnumClass

--- Source/ALS/Public/Utility/AlsEnumUtility.h
+++ Source/ALS/Public/Utility/AlsEnumUtility.h
@@ -2,16 +2,17 @@
 
 #include "Containers/UnrealString.h"
 #include "UObject/ReflectedTypeAccessors.h"
+#include "Templates/IsUEnumClass.h"
 
 namespace AlsEnumUtility
 {
-	template <typename EnumType> requires std::is_enum_v<EnumType>
+	template <typename EnumType> requires TIsUEnumClass<EnumType>::Value
 	int32 GetIndexByValue(const EnumType Value)
 	{
 		return StaticEnum<EnumType>()->GetIndexByValue(static_cast<int64>(Value));
 	}
 
-	template <typename EnumType> requires std::is_enum_v<EnumType>
+	template <typename EnumType> requires TIsUEnumClass<EnumType>::Value
 	FString GetNameStringByValue(const EnumType Value)
 	{
 		return StaticEnum<EnumType>()->GetNameStringByValue(static_cast<int64>(Value));
