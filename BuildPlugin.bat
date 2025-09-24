5407f7eae944a075a0045111767ef591265d4f08
Sixze <16257871+Sixze@users.noreply.github.com>
2025-09-25 00:25:48 +0300

    Increase engine version to 5.7

--- BuildPlugin.bat
+++ BuildPlugin.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set EngineVersion=5.6
+set EngineVersion=5.7
 
 for /f "skip=2 tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\%EngineVersion%" /v "InstalledDirectory"') do set "EngineDirectory=%%b"
 
