ca96f27e6240ea01f0e8541adb274f746b790bc2
Sixze <16257871+Sixze@users.noreply.github.com>
2025-09-25 23:58:59 +0300

    Set IncludeOrderVersion to EngineIncludeOrderVersion.Unreal5_7

--- Source/ALSEditor/ALSEditor.Build.cs
+++ Source/ALSEditor/ALSEditor.Build.cs
@@ -5,7 +5,7 @@ public class ALSEditor : ModuleRules
 	public ALSEditor(ReadOnlyTargetRules target) : base(target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
 		// CppCompileWarningSettings.UnsafeTypeCastWarningLevel = WarningLevel.Warning;
 		CppCompileWarningSettings.NonInlinedGenCppWarningLevel = WarningLevel.Warning;
--- Source/ALSExtras/ALSExtras.Build.cs
+++ Source/ALSExtras/ALSExtras.Build.cs
@@ -5,7 +5,7 @@ public class ALSExtras : ModuleRules
 	public ALSExtras(ReadOnlyTargetRules target) : base(target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
 		// CppCompileWarningSettings.UnsafeTypeCastWarningLevel = WarningLevel.Warning;
 		CppCompileWarningSettings.NonInlinedGenCppWarningLevel = WarningLevel.Warning;
